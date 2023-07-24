a4aa349850251792a1c72b8fee82a91e2612459e
rahmiap4 <2110031805009@sar.ac.id>
2023-07-24 12:17:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/UTS_ANSYORI/QuoteAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.UTS_ANSYORI
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testing.R
+
+class QuoteAdapter(private val listReview: ArrayList<String>) :
+    RecyclerView.Adapter<QuoteAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_quote, viewGroup, false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.tvList.text = listReview[position]
+    }
+    override fun getItemCount(): Int {
+        return listReview.size
+    }
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvList: TextView = view.findViewById(R.id.tvlist)
+    }
+}
